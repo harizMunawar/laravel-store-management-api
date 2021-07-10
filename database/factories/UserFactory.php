fcbb4e63fed00ef50266e5bd91f363f735385163
harizMunawar <munawarhariz@gmail.com>
2021-07-10 17:23:04 +0700

    refactor: change user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,26 +22,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $gender = ['M', 'F'];
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'gender' => $gender[rand(0, 1)],
+            'phone' => '0216854897',
+            'is_superadmin' => false,
+            'store_id' => rand(1, 3),
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
