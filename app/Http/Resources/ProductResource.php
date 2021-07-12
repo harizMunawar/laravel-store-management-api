b56dc7faa9d92a9e850465ffe0b60e25434f17f1
harizMunawar <munawarhariz@gmail.com>
2021-07-12 22:21:57 +0700

    chore: create product resource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\CategoryProduct;
+use App\Http\Resources\StoreResource;
+use App\Http\Resources\CategoryResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=> $this->id,
+            'name'=> $this->name,
+            'description'=> $this->description,
+            'price'=> $this->price,
+            'stock'=> $this->stock,
+            'category'=> CategoryResource::collection($this->categories),
+            'store'=> new StoreResource(Store::find($this->store_id))
+        ];
+    }
+}
